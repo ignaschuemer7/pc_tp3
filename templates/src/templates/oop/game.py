e78d49aa7b3f89b1e5b209e95ead66a5c2a81062
ignacio scgumer <ischumer@udesa.edu.ar>
2022-06-03 09:49:20 -0300

    hay que matar al gnomo para ganar

--- templates/src/templates/oop/game.py
+++ templates/src/templates/oop/game.py
@@ -13,7 +13,7 @@ def game(name):
     dungeon = mapping.Dungeon(ROWS, COLUMNS, 3)
     turns = 0
     #Caracter
-    player1 = Human(name, dungeon.find_free_tile() ,'@',300)
+    player1 = Human(name, dungeon.find_free_tile() ,'@',500)
     #Gnomos
     gnomo1 = Gnomo('gnomo', dungeon.find_free_tile() ,'G',50)
     gnomo2 = Gnomo('gnomo', dungeon.find_free_tile() ,'Ĝ',100)
@@ -24,21 +24,25 @@ def game(name):
     amulet=items.Amulet("amulet",dungeon.find_free_tile())
 
     dungeon.add_item(pickaxe, pickaxe.loc(),1)
-    dungeon.add_item(sword, sword.loc(),2)
-    dungeon.add_item(amulet, amulet.loc(),3)
+    #dungeon.add_item(sword, sword.loc(),2)
+    #dungeon.add_item(amulet, amulet.loc(),3)
     
     while dungeon.level >= 0:
-        '''
-        print(len(dungeon.has_items()))
-        #si el jugador no mata el ultimo gnomo, no obtendra el tesoro
-        if gnomo3.face=='%' and len(dungeon.has_items())<=2:
+        #Put a gnomo in every dungeon
+        gnome=select_gnome(dungeon.level,gnomo1,gnomo2,gnomo3)
+
+        #If the Gnome dies, it face changes (representing its corpse) and the amulet appeard
+        if gnomo_is_dead(gnome) and player1.treasure==False and dungeon.level==2:
+            gnome.face='%'
             dungeon.add_item(amulet, amulet.loc(),3)
-        '''
+        if gnomo_is_dead(gnome) and player1.weapon==False and dungeon.level==1:
+            gnome.face='%'
+            dungeon.add_item(sword, sword.loc(),2)
+        
         turns += 1
         #Game interface
         print('Level:',dungeon.level, str(player1))
-        #Put a gnomo in every dungeon
-        gnome=select_gnome(dungeon.level,gnomo1,gnomo2,gnomo3)
+        
         #Render all the game
         dungeon.render(player1,gnome)
 
@@ -65,10 +69,7 @@ def game(name):
         #if the Player dies, the game ends
         if human_is_dead(player1):
             break
-        #If the Gnome dies, it face changes (representing its corpse)
-        if gnomo_is_dead(gnome):
-            gnome.face='%'
-
+        
     #Out of the principal loop. The game ends 
     if player1.treasure and player1.alive:
         print("You Win!!")

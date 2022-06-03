3f590f11e199252ba1a8a2f7f2f5ec9c94822340
Mateo Giacometti <elmategiaco@gmail.com>
2022-06-03 00:30:50 -0300

    Prueba

--- templates/src/templates/oop/game.py
+++ templates/src/templates/oop/game.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     gnomo1 = Gnomo('gnomo', dungeon.find_free_tile() ,'G',50)
     gnomo2 = Gnomo('gnomo', dungeon.find_free_tile() ,'Ĝ',100)
     gnomo3 = Gnomo('gnomo', dungeon.find_free_tile() ,'ğ',200)
-    #creamos el pico, espada, tesoro
+    #Items
     pickaxe=items.PickAxe("pickacke",dungeon.find_free_tile())
     sword=items.Sword("sword",dungeon.find_free_tile())
     amulet=items.Amulet("amulet",dungeon.find_free_tile())
@@ -46,42 +46,42 @@ if __name__ == "__main__":
             dungeon.add_item(amulet, amulet.loc(),3)
         '''
         turns += 1
-        #mostrar valores en pantalla
+        #Game interface
         print('Level:',dungeon.level, str(player1))
-        #segun el nivel, cada gnomo
+        #Put a gnomo in every dungeon
         gnome=select_gnome(dungeon.level,gnomo1,gnomo2,gnomo3)
-        #renderizamos el juego
+        #Render all the game
         dungeon.render(player1,gnome)
 
         '''
         #print(dungeon.are_connected(player1.loc(), pickaxe.loc()))
         '''
-        #posiciones de los jugadores
+        #Player and Gnome locations
         position_xy_gnomo=gnome.loc()
         position_xy_human=player1.loc()
-        #movimiento y ataques
+        #Movements and attacks (It is not necessary to press the "enter" key every time you have to do an action.)
         key = msvcrt.getch().decode('UTF-8')
-        #movimiento del jugador a partir del valor de key
+        #Player´s movements 
         position_xy_human=player_movements(key,position_xy_human)
-        #movimiento y ataque del jugador al gnomo
+        #Player´s moves and attacks towards the Gnome
         player_move_and_attack(dungeon,player1,gnome,position_xy_human,position_xy_gnomo)
-        #actualizamos posicion
+        #Position update
         position_xy_human=player1.loc()
         position_xy_gnomo=move_gnomo(gnome.loc(),dungeon)
-        #movimiento y ataque del gnomo al jugador
+        #Gnome´s moves and attacks towards the Player
         gnomo_move_and_attack(player1,gnome,position_xy_human,position_xy_gnomo)
-        #condiciones si el jugador agarra los items
+        #Player grabs the items
         pickup(dungeon,player1,pickaxe,sword,amulet)
-        #escaleras
+        #Stairs function
         stairs(dungeon,player1)
-        #si se muere el jugador, termina el juego
+        #if the Player dies, the game ends
         if human_is_dead(player1):
             break
-        #si se muere el gnomo, le cambiamos el caracter, dejamos el cadaver en el mapa
+        #If the Gnome dies, it face changes (representing its corpse)
         if gnomo_is_dead(gnome):
             gnome.face='%'
 
-    # Salió del loop principal, termina el juego
+    #Out of the principal loop. The game ends 
     if player1.treasure and player1.alive:
         print("You Win!!")
     else:
